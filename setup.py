59c2332b494452bef10cab1e16444f1b7489929e
Chitran1987 <ghosal.chitran@gmail.com>
2025-07-09 12:29:22 +0200

    Bumped up python version for Pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='StatsChitran',  # Name of your package
-    version='0.3.7',  # Package version
+    version='0.3.8',  # Package version
     packages=find_packages(),  # Automatically find sub-packages
     install_requires=[
         "numpy",
